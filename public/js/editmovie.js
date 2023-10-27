42b4dfdaacf116356db5929e79963e68332710b1
humphreyokoth <humpasp@gmail.com>
2023-10-27 17:09:08 +0300

    fix:login and register stylings

--- public/js/editmovie.js
+++ public/js/editmovie.js
@@ -1,15 +1,2 @@
-// Function to edit a movie
-function editMovie(movieId, updatedData) {
-    // Make an Axios PUT request to edit the movie with the given ID
-    axios
-      .put(`/editmovie/${movieId}`, updatedData)
-      .then((response) => {
-        // Handling the response and update the UI with the edited movie
-        console.log('Movie edited:', response.data);
-      })
-      .catch((error) => {
-        // Handling any errors
-        console.error('Error editing movie:', error);
-      });
-  }
+
   
