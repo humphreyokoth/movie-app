b8903a364cf58b816fa9ad92e69a0bc0900cc05c
humphreyokoth <humpasp@gmail.com>
2023-10-27 14:50:00 +0300

    fix:getting movies from the database

--- public/js/addmovie.js
+++ public/js/addmovie.js
@@ -1,6 +1,49 @@
-// Function to add a movie
-function addMovie() {
-    // Get input values from the form
+
+  // Function to add a new movie to the database
+  function addMovieToDatabase(title, genre, plot, releaseDate, notes, rating) {
+    // Data to send in the POST request
+    const movieData = {
+      title,
+      genre,
+      plot,
+      releaseDate,
+      notes,
+      rating,
+    };
+  
+    api
+      .post('/addmovie', movieData)
+      .then((response) => {
+        if (response.status === 200) {
+        } else {
+          console.error('Failed to add the movie to the database');
+        }
+      })
+      .catch((error) => {
+        console.error('An error occurred while adding the movie:', error);
+      });
+  }
+  
+  // Event listener for the "Add Movie" button to toggle form visibility
+  const addMovieButton = document.getElementById('add-movie-button');
+  const formContainer = document.getElementById('form-container');
+  
+  addMovieButton.addEventListener('click', function () {
+    // Toggle the visibility of the form
+    if (formContainer.style.display === 'none' || formContainer.style.display === '') {
+      formContainer.style.display = 'block';
+    } else {
+      formContainer.style.display = 'none';
+    }
+  });
+  
+  // Event listener for the movie submission form
+  const movieForm = document.getElementById('addMovieForm');
+  movieForm.addEventListener('submit', function (e) {
+    // Prevent the default form submission
+    e.preventDefault(); 
+  
+    // Get the input values from the form
     const title = document.querySelector('input[name="title"]').value;
     const genre = document.querySelector('input[name="genre"]').value;
     const plot = document.querySelector('input[name="plot"]').value;
@@ -8,26 +51,18 @@ function addMovie() {
     const notes = document.querySelector('input[name="notes"]').value;
     const rating = document.querySelector('input[name="rating"]').value;
   
-    // Create a movie object
-    const newMovie = {
-      title: title,
-      genre: genre,
-      plot: plot,
-      releaseDate: releaseDate,
-      notes: notes,
-      rating: rating,
-    };
+    // Make a POST request to add the movie to the database using Axios
+    addMovieToDatabase(title, genre, plot, releaseDate, notes, rating);
   
-    // Make an Axios POST request to add the movie to the backend
-    axios
-      .post('/addmovie', newMovie)
-      .then((response) => {
-        // Handling the response and update the UI with the added movie
-        console.log('Movie added:', response.data);
-      })
-      .catch((error) => {
-       // Handling any errors
-        console.error('Error adding movie:', error);
-      });
-  }
+    // Clear the form inputs
+    document.querySelector('input[name="title"]').value = '';
+    document.querySelector('input[name="genre"]').value = '';
+    document.querySelector('input[name="plot"]').value = '';
+    document.querySelector('input[name="releaseDate"]').value = '';
+    document.querySelector('input[name="notes"]').value = '';
+    document.querySelector('input[name="rating"]').value = '';
+  
+    // Hide the form
+    formContainer.style.display = 'none';
+  });
   
---
+++ public/js/getMovies.js
@@ -0,0 +1,76 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Select the movie listings container
+    const movieList = document.getElementById('movie-listings');
+  
+    // Function to get movies and update the movie listings
+    function getAllMoviesAndUpdate() {
+      // Make an API request to fetch movies from the database
+      api.get('/getmovies')
+        .then(response => {
+          // Access the array of movies using response.data.getMovieList
+          const movies = response.data.getMovieList;
+  
+          // Check if 'movies' is an array before iterating
+          if (Array.isArray(movies)) {
+            // Clear the current movie listings
+            movieList.innerHTML = '';
+  
+            // Use a for loop to create movie elements
+            for (let i = 0; i < movies.length; i++) {
+              const movie = movies[i];
+  
+              const movieElement = document.createElement('div');
+              movieElement.classList.add('movie');
+  
+              // Create and set the content for movie details
+              const img = document.createElement('img');
+              img.src = movie.posterUrl || '#'; 
+              img.alt = 'Movie poster';
+  
+              const title = document.createElement('h2');
+              title.textContent = movie.title;
+  
+              const editIcon = document.createElement('i');
+              editIcon.classList.add('fas', 'fa-edit', 'edit-icon');
+  
+              const deleteIcon = document.createElement('i');
+              deleteIcon.classList.add('fas', 'fa-trash', 'delete-icon');
+  
+              const rating = document.createElement('div');
+              rating.classList.add('rating');
+              for (let j = 0; j < 5; j++) {
+                const star = document.createElement('i');
+                star.classList.add('fas', 'fa-star');
+                if (j >= movie.rating) {
+                  star.classList.remove('fa-star');
+                  star.classList.add('far', 'fa-star');
+                }
+                rating.appendChild(star);
+              }
+  
+              // Append the elements to the movie element
+              movieElement.appendChild(img);
+              movieElement.appendChild(title);
+              movieElement.appendChild(editIcon);
+              movieElement.appendChild(deleteIcon);
+              movieElement.appendChild(rating);
+  
+              // Append the movie element to the movie listings
+              movieList.appendChild(movieElement);
+            }
+          } else {
+            // Handle the case where 'movies' is not an array
+            console.error('The response data is not an array of movies.');
+          }
+        })
+        .catch(error => {
+          console.error(error);
+          movieList.innerHTML = '<p>Failed to load movies. Try again later.</p>';
+        });
+    }
+  
+    // Call the function on page load to fetch and display movies
+    
+    getAllMoviesAndUpdate()
+  });
+  
