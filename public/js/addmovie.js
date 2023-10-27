43714844b2a130c3680e828ebdd794f3bbfa5f5c
humphreyokoth <humpasp@gmail.com>
2023-10-27 15:14:38 +0300

    fix:getting movies from the database

--- public/js/addmovie.js
+++ public/js/addmovie.js
@@ -1,6 +1,5 @@
-
-  // Function to add a new movie to the database
-  function addMovie(title, genre, plot, releaseDate, notes, rating) {
+// Function to add a new movie to the database
+function addMovie(title, genre, plot, releaseDate, notes, rating) {
     // Data to send in the POST request
     const movieData = {
       title,
@@ -15,6 +14,8 @@
       .post('/addmovie', movieData)
       .then((response) => {
         if (response.status === 200) {
+          // Movie added successfully, now add it to the list
+          addMovieToList(title, genre, plot, releaseDate, notes, rating);
         } else {
           console.error('Failed to add the movie to the database');
         }
@@ -22,6 +23,21 @@
       .catch((error) => {
         console.error('An error occurred while adding the movie:', error);
       });
+  }
+  
+  // Function to add a movie to the list
+  function addMovieToList(title, genre, plot, releaseDate, notes, rating) {
+    const movieList = document.querySelector('.movie-listings');
+    const movieDiv = document.createElement('div');
+    movieDiv.classList.add('movie');
+  
+    const movieTitle = document.createElement('h2');
+    movieTitle.textContent = title;
+  
+    // Create other elements (image, edit, delete, rating) and add them to 'movieDiv'
+  
+    movieDiv.appendChild(movieTitle);
+    movieList.appendChild(movieDiv);
   }
   
   // Event listener for the "Add Movie" button to toggle form visibility
@@ -41,7 +57,7 @@
   const movieForm = document.getElementById('addMovieForm');
   movieForm.addEventListener('submit', function (e) {
     // Prevent the default form submission
-    e.preventDefault(); 
+    e.preventDefault();
   
     // Get the input values from the form
     const title = document.querySelector('input[name="title"]').value;
